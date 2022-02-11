93c35b9ea3ac658974fe4b50cd9dcefe4f15f241
irenekyc <gugu_irene@hotmail.com>
2022-02-12 06:30:51 +0800

    update env config file

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix:
-    process.env.NODE_ENV !== "development" ? "/space-travel-teaser-site/" : "",
+    process.env.NODE_ENV !== "development" ? "/space-travel-teaser-site" : "",
   images: {
     loader: "akamai",
     path: "",
+  },
+  basePath: "/space-travel-teaser-site",
+  env: {
+    BACKEND_URL: "/space-travel-teaser-site",
   },
 };
 
