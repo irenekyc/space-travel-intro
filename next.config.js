44931405c296eab0bbd09fa60ea28037015e4803
irenekyc <gugu_irene@hotmail.com>
2022-02-12 06:24:09 +0800

    deployment update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV !== "development" ? "/space-travel-teaser-site/" : "",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
